a41a95e2b107f161c82637e8a768ad76f6ef6e7a
“udit01” <uditjainiitd@gmail.com>
2017-12-05 05:02:26 +0530

    on interweaving the other way, we get inefficient use of lazy sequencing and not use of optimizations.

---
+++ Linq/Extension.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+
+namespace LinqFaroShuffle
+{
+    public static class Extensions
+    {
+        public static IEnumerable<T> InterleaveSequenceWith<T>
+            (this IEnumerable<T> first, IEnumerable<T> second)
+        {
+            var firstIter = first.GetEnumerator();
+            var secondIter = second.GetEnumerator();
+
+            //what if the while condition becomes false when what do we retrun ? only 1 cut remaining?
+            while (firstIter.MoveNext() && secondIter.MoveNext())
+            {
+                yield return firstIter.Current;
+                yield return secondIter.Current;
+            }   
+        }
+
+        public static bool SequenceEquals<T>
+            (this IEnumerable<T> first, IEnumerable<T> second)
+        {
+            /*What if differnet length of sequences ? Does this still work ? */
+            var firstIter = first.GetEnumerator();
+            var secondIter = second.GetEnumerator();
+
+            while (firstIter.MoveNext() && secondIter.MoveNext())
+            {
+                if (!firstIter.Current.Equals(secondIter.Current))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
--- Linq/Program.cs
+++ Linq/Program.cs
@@ -2,7 +2,7 @@
 using System.Linq;
 using System.Collections.Generic;
 
-namespace Linq
+namespace LinqFaroShuffle
 {
     class Program
     {
@@ -13,12 +13,50 @@ namespace Linq
             var startingDeck = from s in Suits()
                                from r in Ranks()
                                select new { Suit = s , Rank = r};
-
+            /*What's the type of the deck ? */
+            Console.WriteLine("Printing the starting deck::::::::::::::::");
             foreach(var c in startingDeck){
                 Console.WriteLine(c);
             }
+            
+            var top = startingDeck.Take(26);
+            var bottom = startingDeck.Skip(26);
+            // var shuffle = top.InterleaveSequenceWith(bottom);
+
+            // Console.WriteLine("Printing the shuffeled deck::::::::::::::::");
+            // foreach(var c in shuffle){
+            //     Console.WriteLine(c);
+            // }
+
+            var times = 0;
+            var shuffle = startingDeck;
+
+            do
+            {
+                // shuffle = shuffle.Take(26).InterleaveSequenceWith(shuffle.Skip(26));
+                shuffle = shuffle.Skip(26).InterleaveSequenceWith(shuffle.Take(26));
+                //2nd line is very painful and hangs the system
+                foreach (var c in shuffle)
+                {
+                    Console.WriteLine(c);
+                }
+
+                Console.WriteLine();
+                times++;
+            } while (!startingDeck.SequenceEquals(shuffle));
+
+            Console.WriteLine(times);
+
         }
 
+        // static void PrintDeck(var startingDeck){
+
+        //     foreach (var c in startingDeck)
+        //     {
+        //         Console.WriteLine(c);
+        //     }
+
+        // }
         /*Iterator methods below */
         static IEnumerable<string> Suits()
         {
