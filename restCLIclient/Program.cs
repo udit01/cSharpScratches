0104940fa33f3509fc8d0682b161602e685d5b3d
“udit01” <uditjainiitd@gmail.com>
2017-12-04 07:13:15 +0530

    pinged github api to get info of a user handle by httpClient with specific headers

--- restCLIclient/Program.cs
+++ restCLIclient/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
-namespace restCLIclient
-{
+namespace WebAPIClient
+{   
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ProcessRepositories().Wait();
+        }
+
+        private static async Task ProcessRepositories(){
+            var client = new HttpClient();
+
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var stringTask = client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+
+            var msg = await stringTask;
+            Console.Write(msg);
         }
     }
 }
