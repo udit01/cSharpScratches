747e5c7a7a96dcb4668f4829d1521979e901a873
“udit01” <uditjainiitd@gmail.com>
2017-12-04 21:55:34 +0530

    reading from file at human readable speed

--- IOApi/Program.cs
+++ IOApi/Program.cs
@@ -1,12 +1,53 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
-namespace IOApi
+
+namespace TeleprompterConsole
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Start of TeleprompterConsole App");
+
+            var lines = ReadFrom("sampleQuotes.txt");
+            foreach(var line in lines){
+
+                //Console.WriteLine(line);
+
+                Console.Write(line);
+                if (!string.IsNullOrWhiteSpace(line))
+                {
+                    var pause = Task.Delay(100);
+                    // Synchronously waiting on a task is an
+                    // anti-pattern. This will get fixed in later
+                    // steps.
+                    pause.Wait();
+                }
+
+            }
+        }
+
+        static IEnumerable<string> ReadFrom(string FileName){
+            string line;
+            using(var reader = File.OpenText(FileName)){//reader is a StreamReader object , decide at compile time
+                while( (line = reader.ReadLine()) != null){
+                    
+                    var words = line.Split(' ');
+                    foreach (var word in words)
+                    {
+                        yield return word + " ";
+                    }
+                    yield return Environment.NewLine;
+                    
+                    /*
+                    yield return line;//What the hell is this ?
+                    //lazy return reads when required/called by the code to do so.
+                     */
+                }
+            }
         }
     }
 }
