2b552d0e055ff1ffc3af143049035ba27fe11c41
“udit01” <uditjainiitd@gmail.com>
2017-12-04 07:29:28 +0530

    streamed the names of repos, converted Json data to c# objects by serializer

---
+++ readme.md
@@ -0,0 +1,1 @@
+Repository containing my CSharp projects
--- restCLIclient/Program.cs
+++ restCLIclient/Program.cs
@@ -2,6 +2,8 @@
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Json;
 
 namespace WebAPIClient
 {   
@@ -20,10 +22,20 @@ namespace WebAPIClient
                                 new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
             client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
 
-            var stringTask = client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+            var serializer = new DataContractJsonSerializer(typeof(List<repo>));
 
+            var streamTask = client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+            var repositories = serializer.ReadObject(await streamTask) as List<repo>;
+
+            /*
+            var stringTask = client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
             var msg = await stringTask;
-            Console.Write(msg);
+            // Console.Write(msg);
+             */
+
+            foreach (var repo in repositories)
+                Console.WriteLine(repo.name);
+
         }
     }
 }
